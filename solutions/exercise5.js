c9f434d922fd4e57dc16b37f8dbe583d10f4a633
Miguel González <migohe14@gmail.com>
2019-03-27 13:55:30 +0100

    e5 and e6

--- solutions/exercise5.js
+++ solutions/exercise5.js
@@ -1,6 +1,10 @@
 function checkUsersValid(goodUsers) {
     return function allUsersValid(submittedUsers) {
-      // SOLUTION GOES HERE
+      return submittedUsers.every((submittedUser) => {
+        return goodUsers.some((goodUser) => {
+          return goodUser.id === submittedUser.id
+        })
+      })
     };
   }
 
