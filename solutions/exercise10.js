bfe430261f81da060a34b53eb4f50fa7810759b9
Miguel González <migohe14@gmail.com>
2019-04-01 08:29:03 +0200

    e10 and e11

---
+++ solutions/exercise10.js
@@ -0,0 +1,8 @@
+module.exports = function arrayMap(arr, fn, thisArg) {
+    console.log(fn)
+    return arr.reduce(function(acc, item, index, arr) {
+      acc.push(fn.call(thisArg, item, index, arr))
+      return acc
+    }, [])
+  }
+  
